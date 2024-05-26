df3821db6beee73693c218ca76ad829b0e39e1d2
abc <qbao.hrt@gmail.com>
2024-05-27 00:34:55 +0700

    abc

--- DialogflowCX/webhook.py
+++ DialogflowCX/webhook.py
@@ -2,6 +2,10 @@ from flask import Flask, request, jsonify
 import json
 from datetime import datetime
 import subprocess
+import requests
+
+# OpenWeatherMap API key
+API_KEY = '2c430b975eca3a7ccf32915d7a02fb28'
 
 def start_tunnel():
     command = 'autossh -M 0 -o ServerAliveInterval=60 -i DialogflowCX/key/id_rsa -R iotchallenge.rcuet.id.vn:80:localhost:5000 serveo.net'
@@ -21,6 +25,17 @@ def responseMgs(messages): # The function writes the response json structure for
             }
         }
     )
+
+def get_weather(city):
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        temp = data['main']['temp']
+        description = data['weather'][0]['description']
+        return f"Nhiệt độ hiện tại ở {city} là {temp} độ C với {description}."
+    else:
+        return "Không thể lấy thông tin thời tiết cho thành phố này."
 
 app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
@@ -43,9 +58,12 @@ def dialogflow():
 
             messages = f"Bây giờ là {hour} giờ {minute} phút ngày {day} tháng {month} năm {year}"
 
+        elif webhook_tag == "askWeather": # Intent Ask about the weather
+            city = data["sessionInfo"]["parameters"]["geo-city"]
+            messages = get_weather(city)
+
         return responseMgs(messages)
     
-
 
 if __name__ == "__main__":
     start_tunnel()
