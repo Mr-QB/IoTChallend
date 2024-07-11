88507281314430168571ad1a0d140f6a2c8c043d
Cai <qbao1607@gmail.com>
2024-07-11 21:05:26 +0700

    fix ssh serveo

--- ControlApp/AppControl.py
+++ ControlApp/AppControl.py
@@ -1,11 +1,13 @@
 import json
 import paho.mqtt.client as mqtt
 from flask import Flask, jsonify, request
+import subprocess
 from threading import Thread
 
 
 class AppControl:
     def __init__(self):
+        self.startTunnel()
         self.app = Flask(__name__)
 
     def setUpMqtt(self, broker, mqtt_port=1883, mqtt_username="", mqtt_password=""):
@@ -21,6 +23,10 @@ class AppControl:
         self.mqtt_client.username_pw_set(self.mqtt_username, self.mqtt_password)
         self.mqtt_client.on_connect = on_connect
         self.mqtt_client.connect(broker, self.mqtt_port, 60)
+
+    def startTunnel(self):
+        command = "autossh -M 0 -o ServerAliveInterval=60 -i DialogflowCX/key/id_ssh -R iotchallengemqtt.rcuet.id.vn:80:localhost:5000 serveo.net"
+        subprocess.Popen(command, shell=True)
 
     def sentMQTTMsg(self, topic, msg):
         self.mqtt_client.publish(topic, msg, 0, True)
