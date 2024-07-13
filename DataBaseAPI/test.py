df47c3d700b0a71f0947d3f805cec268779e567a
Cai <qbao1607@gmail.com>
2024-07-13 14:02:02 +0700

    update tts

--- DataBaseAPI/test.py
+++ DataBaseAPI/test.py
@@ -1,14 +1,18 @@
 import pandas as pd
 from pymongo import MongoClient
 import numpy as np
+from random import randint
+
+# id = str(randint(1000, 5000))
 
 # Kết nối tới MongoDB
 client = MongoClient("mongodb://localhost:27017/")
 db = client["RcuetProject"]  # Tên cơ sở dữ liệu của bạn
 face_collection = db["faces_data"]  # Tên bộ sưu tập của bạn
+devices_collection = db["Devices"]  # Tên bộ sưu tập của bạn
 
-cursor = face_collection.find({})
-data_list = list(cursor)
-face_data = pd.DataFrame(data_list)
-face_data["embedding"] = face_data["embedding"].apply(np.array)
-print(face_data)
+id = randint(1000, 5000)
+cursor = devices_collection.find({})
+
+new_device = {"device_name": "quạt", "room_name": "phòng ngủ", "id": id}
+devices_collection.insert_one(new_device)
